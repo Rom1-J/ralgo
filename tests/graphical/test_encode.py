bd45d13d811be7c971284eb36492375c0ddf03b2
Romain J <romain.ordi@gmail.com>
2021-02-21 17:44:38 +0100

    feat(tests): add tests to graphical encoder

---
+++ tests/graphical/test_encode.py
@@ -0,0 +1,39 @@
+import os
+
+from PIL import Image
+from faker import Faker
+
+from ralgo.ralgo import Ralgo
+
+fake = Faker()
+
+
+def test_encode_graphical_basic():
+    message = "Salut"
+    path = f"tests/graphical/tmp/{fake.pyint()}.png"
+
+    encoded = Ralgo(message).encode().graphical().encode()
+
+    encoded.save(path)
+
+    img = Image.open(path)
+
+    assert img.size == (17, 17)
+
+    os.remove(path)
+
+    # =======================
+
+    message = fake.text()
+    path = f"tests/graphical/tmp/{fake.pyint()}.png"
+
+    encoded = Ralgo(message).encode().graphical().encode()
+
+    encoded.save(path)
+
+    img = Image.open(path)
+
+    assert img.size == (17, 17)
+
+    os.remove(path)
+    
