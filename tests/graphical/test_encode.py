074a430c74bec1941d5fef1351a623f6627f1694
Romain J <romain.ordi@gmail.com>
2021-02-21 18:07:11 +0100

    feat(tests): add more tests to graphical encoder

--- tests/graphical/test_encode.py
+++ tests/graphical/test_encode.py
@@ -1,5 +1,6 @@
 import math
 import os
+from io import BytesIO
 
 from PIL import Image
 from faker import Faker
@@ -10,6 +11,23 @@ fake = Faker()
 
 
 def test_encode_graphical_basic():
+    message = "Salut"
+
+    _ = Ralgo(message).encode().graphical().encode()
+
+    assert True  # this test should not fail
+
+    # =======================
+
+    message = fake.text()
+
+    encoded = Ralgo(message).encode()
+    _ = encoded.graphical().encode()
+
+    assert True  # this test should not fail
+
+
+def test_encode_graphical_save():
     message = "Salut"
     path = f"tests/graphical/tmp/{fake.pyint()}.png"
 
@@ -41,3 +59,31 @@ def test_encode_graphical_basic():
     )
 
     os.remove(path)
+
+
+def test_encode_graphical_bytes():
+    message = "Salut"
+
+    encoded = Ralgo(message).encode().graphical().encode()
+
+    arr = encoded.to_bytes()
+
+    img = Image.open(BytesIO(arr))
+
+    assert img.size == (17, 17)
+
+    # =======================
+
+    message = fake.text()
+
+    encoded = Ralgo(message).encode()
+    graphical = encoded.graphical().encode()
+
+    arr = graphical.to_bytes()
+
+    img = Image.open(BytesIO(arr))
+
+    assert img.size == (
+        math.ceil(math.sqrt(len(encoded))),
+        math.ceil(math.sqrt(len(encoded))),
+    )
